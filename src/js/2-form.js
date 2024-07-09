dc101821060d8126c0085edba3a79f5a69169cf0
Alina <alinazolotopup99@gmail.com>
2024-07-09 23:46:42 +0200

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -14,18 +14,19 @@ const formEL = {
   function populateTextareaField() {
     const savedFormData = JSON.parse(localStorage.getItem('feedback-form-state'));
     if (savedFormData) {
-      email.value = savedFormData.email || '';
-      message.value = savedFormData.message || '';
+      email.value = savedFormData.email.trim() || '';
+      message.value = savedFormData.message.trim() || '';
+      formData = savedFormData;
     }
   }
   
   function onFormSubmit(e) {
     e.preventDefault();
-    if (email.value === "" || message.value === "") {
+    if (email.value.trim() === "" || message.value.trim() === "") {
       alert('Please fill all fields');
       return;
     }
-    console.log('Data sent successfully');
+    console.log('Form data:', formData);
     e.currentTarget.reset();
     localStorage.removeItem('feedback-form-state');
   }
@@ -33,25 +34,13 @@ const formEL = {
   function onFormInput(event) {
     event.preventDefault();
     formData[event.target.name] = event.target.value;
-    if (email.value === "" || message.value === "") {
-      return;
-    }
-    console.log(formData);
     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  console.log(formData);
   }
   
   form.addEventListener('submit', onFormSubmit);
   form.addEventListener('input', onFormInput);
   
-  document.addEventListener('DOMContentLoaded', populateTextareaField);
-  
-  
-  // function onTextareaInput(event) {
-  //   formData.email = email.value;
-  //   formData.message = message.value;
-  //   console.log(event.target.value);
-  //   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-  // }
   
   document.addEventListener('DOMContentLoaded', populateTextareaField);
   
